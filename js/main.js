28fccf2fea61f884c1b7c458f4fe1b5d456545cb
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2025-09-22 12:55:13 +0530

    done all pages

--- js/main.js
+++ js/main.js
@@ -1627,7 +1627,7 @@
             dots: false,
             infinite: true,
             speed: 300,
-            slidesToShow: 3,
+            slidesToShow: 4,
             slidesToScroll: 1,
             prevArrow: '<a class="slick-prev"><i class="fas fa-arrow-left" alt="Arrow Icon"></i></a>',
             nextArrow: '<a class="slick-next"><i class="fas fa-arrow-right" alt="Arrow Icon"></i></a>',
